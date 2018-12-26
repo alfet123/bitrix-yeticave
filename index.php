2b1440cb18b7c26ca2ce404213f57f9b7cd24ceb
Alexey Fetisov <alfet123@gmail.com>
2018-12-27 01:34:05 +0500

    Интеграция главной страницы

--- index.php
+++ index.php
@@ -68,10 +68,10 @@ $APPLICATION->SetTitle("Главная");
 		"CACHE_GROUPS" => "N",
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "N",
-		"COMPATIBLE_MODE" => "Y",
+		"COMPATIBLE_MODE" => "N",
 		"DETAIL_URL" => "",
 		"DISABLE_INIT_JS_IN_COMPONENT" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_BOTTOM_PAGER" => "N",
 		"DISPLAY_COMPARE" => "N",
 		"DISPLAY_TOP_PAGER" => "N",
 		"ELEMENT_SORT_FIELD" => "id",
@@ -83,8 +83,7 @@ $APPLICATION->SetTitle("Главная");
 		"IBLOCK_ID" => "1",
 		"IBLOCK_TYPE" => "lots",
 		"INCLUDE_SUBSECTIONS" => "Y",
-		"LABEL_PROP" => array(
-		),
+		"LABEL_PROP" => "",
 		"LAZY_LOAD" => "N",
 		"LINE_ELEMENT_COUNT" => "3",
 		"LOAD_ON_SCROLL" => "N",
@@ -103,11 +102,11 @@ $APPLICATION->SetTitle("Главная");
 		"PAGER_SHOW_ALL" => "N",
 		"PAGER_SHOW_ALWAYS" => "N",
 		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Товары",
+		"PAGER_TITLE" => "Лоты",
 		"PAGE_ELEMENT_COUNT" => "18",
 		"PARTIAL_PRODUCT_PROPERTIES" => "N",
 		"PRICE_CODE" => array(
-			0 => "price",
+			0 => "baseprice",
 		),
 		"PRICE_VAT_INCLUDE" => "Y",
 		"PRODUCT_BLOCKS_ORDER" => "price,props,sku,quantityLimit,quantity,buttons",
@@ -122,8 +121,9 @@ $APPLICATION->SetTitle("Главная");
 		"SECTION_ID_VARIABLE" => "SECTION_ID",
 		"SECTION_URL" => "",
 		"SECTION_USER_FIELDS" => array(
-			0 => "price",
+			0 => "",
 			1 => "amount",
+			2 => "",
 		),
 		"SEF_MODE" => "N",
 		"SET_BROWSER_TITLE" => "N",
@@ -144,7 +144,11 @@ $APPLICATION->SetTitle("Главная");
 		"USE_MAIN_ELEMENT_SECTION" => "N",
 		"USE_PRICE_COUNT" => "N",
 		"USE_PRODUCT_QUANTITY" => "N",
-		"COMPONENT_TEMPLATE" => "yeticave_catalog"
+		"COMPONENT_TEMPLATE" => "yeticave_catalog",
+		"CUSTOM_FILTER" => "",
+		"HIDE_NOT_AVAILABLE" => "N",
+		"HIDE_NOT_AVAILABLE_OFFERS" => "N",
+		"CONVERT_CURRENCY" => "N"
 	),
 	false
 );?>
--- local/templates/yeticave/components/bitrix/catalog.section/yeticave_catalog/template.php
+++ local/templates/yeticave/components/bitrix/catalog.section/yeticave_catalog/template.php
@@ -1,6 +1,26 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
 
 <?
+function getAmountString($amount)
+{
+	$amountString = "";
+
+	if ($amount == 0)
+	{
+		$amountString = "Стартовая цена";
+	}
+	else
+	{
+		$amountLastNumber = (int)substr((string)$amount, -1);
+		$amountPreLastNumber = ($amount > 9) ? (int)substr((string)$amount, -2, 1) : 0;
+		if ($amountLastNumber == 0 || $amountLastNumber >= 5 || $amountPreLastNumber == 1) $amountString = $amount." ставок";
+		elseif ($amountLastNumber == 1) $amountString = $amount." ставка";
+		else $amountString = $amount." ставки";
+	}
+	
+	return $amountString;
+}
+
 function getTimerString($dateTimeString)
 {
 	$currentTime = Time();
@@ -34,15 +54,19 @@ function getTimerString($dateTimeString)
 	<?foreach($arResult['ITEMS'] as $arItem):?>
 		<li class="lots__item lot">
 			<div class="lot__image">
-				<img src="<?=$arItem[DETAIL_PICTURE][SRC]?>" width="350" height="260" alt="Категория">
+				<img src="<?=$arItem[DETAIL_PICTURE][SRC]?>" width="350" height="260" alt="<?=$arItem[DETAIL_PICTURE][ALT]?>">
 			</div>
 			<div class="lot__info">
 				<span class="lot__category">Категория</span>
 				<h3 class="lot__title"><a class="text-link" href="lot.html"><?=$arItem[NAME]?></a></h3>
 				<div class="lot__state">
 					<div class="lot__rate">
-						<span class="lot__amount">12 ставок</span>
-						<span class="lot__cost">15 999<b class="rub">р</b></span>
+						<span class="lot__amount">
+							<? echo getAmountString($arItem[PROPERTIES][amount][VALUE]); ?>
+						</span>
+						<span class="lot__cost">
+							<? echo number_format($arItem[ITEM_PRICES][0][PRICE], 0, '', ' '); ?><b class="rub">р</b>
+						</span>
 					</div>
 					<div class="lot__timer timer">
 						<? echo getTimerString($arItem[DATE_ACTIVE_FROM]); ?>
