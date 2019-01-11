b32d8881c26ac70a77a86b4722413c37290f3b37
Alexey Fetisov <alfet123@gmail.com>
2019-01-11 20:00:37 +0500

    Интеграция главной страницы

--- local/templates/yeticave/components/bitrix/catalog.section/yeticave_catalog/result_modifier.php
+++ local/templates/yeticave/components/bitrix/catalog.section/yeticave_catalog/result_modifier.php
@@ -3,6 +3,23 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 if (!empty($arResult))
 {
+	// Получить список всех разделов
+
+	$arOrder = Array("ID"=>"ASC");
+	$arFilter = Array("IBLOCK_ID"=>"1");
+	$arSelect = Array("ID", "NAME");
+	$res = CIBlockSection::GetList($arOrder, $arFilter, false, $arSelect, false);
+
+	$yeticaveCategory = array();
+	while($ob = $res->GetNextElement())
+	{
+		$arFields = $ob->GetFields();
+		$id = $arFields["ID"];
+		$yeticaveCategory[$id] = $arFields["NAME"];
+	}
+
+	// Получить список всех элементов
+
 	$arOrder = Array("ID"=>"ASC");
 	$arFilter = Array("IBLOCK_ID"=>"1");
 	$arSelect = Array("ID", "IBLOCK_SECTION_ID");
@@ -16,11 +33,13 @@ if (!empty($arResult))
 		$category[$id] = $arFields["IBLOCK_SECTION_ID"];
 	}
 
+	// Установить названия разделов для всех элементов
+
 	for($i = 0; $i < count($arResult["ITEMS"]); $i++)
 	{
 		$id = $arResult["ITEMS"][$i]["ID"];
 		$categoryId = $category[$id];
-		$arResult["ITEMS"][$i]["CATEGORY"] = $GLOBALS["YETICAVE_CATEGORY"][$categoryId];
+		$arResult["ITEMS"][$i]["CATEGORY"] = $yeticaveCategory[$categoryId];
 	}
 }
 ?>
