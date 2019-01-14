e8a7730b32711bfb7f332c5f880f8b60ef63a366
Alexey Fetisov <alfet123@gmail.com>
2019-01-14 21:20:05 +0500

    Интеграция главной страницы

--- local/templates/yeticave/components/bitrix/catalog.section/yeticave_catalog/result_modifier.php
+++ local/templates/yeticave/components/bitrix/catalog.section/yeticave_catalog/result_modifier.php
@@ -3,43 +3,52 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 if (!empty($arResult))
 {
-	// Получить список всех разделов
+	// Массив ID элементов
 
-	$arOrder = Array("ID"=>"ASC");
-	$arFilter = Array("IBLOCK_ID"=>"1");
-	$arSelect = Array("ID", "NAME");
-	$res = CIBlockSection::GetList($arOrder, $arFilter, false, $arSelect, false);
-
-	$yeticaveCategory = array();
-	while($ob = $res->GetNextElement())
-	{
-		$arFields = $ob->GetFields();
-		$id = $arFields["ID"];
-		$yeticaveCategory[$id] = $arFields["NAME"];
+	$arItemsIds = array();
+	foreach($arResult["ITEMS"] as $item){
+		$arItemsIds[] = $item["ID"];
 	}
 
-	// Получить список всех элементов
+	// Получить список элементов $arItems в виде "ID элемента" => "ID раздела"
 
-	$arOrder = Array("ID"=>"ASC");
-	$arFilter = Array("IBLOCK_ID"=>"1");
+	$arOrder = Array("ID" => "ASC");
+	$arFilter = Array("IBLOCK_ID" => "1", "ID" => $arItemsIds);
 	$arSelect = Array("ID", "IBLOCK_SECTION_ID");
 	$res = CIBlockElement::GetList($arOrder, $arFilter, false, false, $arSelect);
 
-	$category = array();
+	$arItems = array();
 	while($ob = $res->GetNextElement())
 	{
 		$arFields = $ob->GetFields();
 		$id = $arFields["ID"];
-		$category[$id] = $arFields["IBLOCK_SECTION_ID"];
+		$arItems[$id] = $arFields["IBLOCK_SECTION_ID"];
 	}
 
-	// Установить названия разделов для всех элементов
+	// Получить список разделов $arSections в виде "ID раздела" => "Название раздела"
+
+	$arOrder = Array("ID" => "ASC");
+	$arFilter = Array("IBLOCK_ID" => "1", "ID" => array_unique($arItems));
+	$arSelect = Array("ID", "NAME");
+	$res = CIBlockSection::GetList($arOrder, $arFilter, false, $arSelect, false);
+
+	$arSections = array();
+	while($ob = $res->GetNextElement())
+	{
+		$arFields = $ob->GetFields();
+		$id = $arFields["ID"];
+		$arSections[$id] = $arFields["NAME"];
+	}
+
+	// Установить "ID раздела" и "Название раздела" для элементов
 
 	for($i = 0; $i < count($arResult["ITEMS"]); $i++)
 	{
-		$id = $arResult["ITEMS"][$i]["ID"];
-		$categoryId = $category[$id];
-		$arResult["ITEMS"][$i]["CATEGORY"] = $yeticaveCategory[$categoryId];
+		$itemId = $arResult["ITEMS"][$i]["ID"];
+		$sectionId = $arItems[$itemId];
+		$sectionName = $arSections[$sectionId];
+		$arResult["ITEMS"][$i]["SECTION_ID"] = $sectionId;
+		$arResult["ITEMS"][$i]["SECTION_NAME"] = $sectionName;
 	}
 }
 ?>
--- local/templates/yeticave/components/bitrix/catalog.section/yeticave_catalog/template.php
+++ local/templates/yeticave/components/bitrix/catalog.section/yeticave_catalog/template.php
@@ -57,7 +57,7 @@ function getTimerString($dateTimeString)
 				<img src="<?=$arItem[DETAIL_PICTURE][SRC]?>" width="350" height="260" alt="<?=$arItem[DETAIL_PICTURE][ALT]?>">
 			</div>
 			<div class="lot__info">
-				<span class="lot__category"><?=$arItem[CATEGORY]?></span>
+				<span class="lot__category"><?=$arItem[SECTION_NAME]?></span>
 				<h3 class="lot__title"><a class="text-link" href="lot.html"><?=$arItem[NAME]?></a></h3>
 				<div class="lot__state">
 					<div class="lot__rate">
